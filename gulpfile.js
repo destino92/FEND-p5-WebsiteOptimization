306be3e46185577e929afa476b3bdd470fc321c8
destino92 <destinodello@gmail.com>
2016-04-04 02:41:04 +0300

    require gulp-sourcemaps module

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,6 @@ var gulp = require('gulp'),
 	cleanCSS = require('gulp-clean-css'),
 	uglify = require('gulp-uglify'),
 	concatify = require('gulp-concat'),
-	imageop = require('gulp-image-optimization');
+	imageop = require('gulp-image-optimization'),
+	sourcemaps = require('gulp-sourcemaps');
 
