5251a66c87ea2b6f78f48c87149a66e9bc59435a
destino92 <destinodello@gmail.com>
2016-04-06 11:39:28 +0300

    remove some variable to be cached outside of the loop inside updatePositions

--- views/js/main.js
+++ views/js/main.js
@@ -497,14 +497,18 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
+// constant variable to be used inside updatePositions
+var items = document.getElementsByClassName('mover'),
+    moversLength = items.length
+
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
+  var top = document.body.scrollTop;
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.getElementsByClassName('mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  for (var i = 0; i < moversLength; i++) {
+    var phase = Math.sin((top / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
